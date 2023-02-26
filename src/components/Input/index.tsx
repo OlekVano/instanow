384d782a83c3abdeb595c69032aeb8e7bc13f816
OlekVano <olekvano@gmail.com>
2023-02-26 00:13:10 +0000

    Removed defaultValue from inputs and textareas

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,12 @@ import styles from './index.module.scss'
 
 type Props = {
   placeholder?: string,
-  defaultValue?: string,
   value?: string,
   func?: Function
 }
 
-export default function Input({ placeholder='', defaultValue='', value=defaultValue, func=function(){} }: Props) {
+export default function Input({ placeholder='', value='', func=function(){} }: Props) {
   return (
-    <input className={styles.main} type='text' defaultValue={defaultValue} value={value} placeholder={placeholder} onChange={func as ChangeEventHandler<HTMLInputElement>} />
+    <input className={styles.main} type='text' value={value} placeholder={placeholder} onChange={func as ChangeEventHandler<HTMLInputElement>} />
   )
 }
--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -3,13 +3,12 @@ import styles from './index.module.scss'
 
 type Props = {
   placeholder?: string,
-  defaultValue?: string,
   value?: string,
   func: Function
 }
 
-export default function Textarea({ placeholder='', defaultValue='', value=defaultValue, func=function(){} }: Props) {
+export default function Textarea({ placeholder='', value='', func=function(){} }: Props) {
   return (
-    <textarea className={styles.main} defaultValue={defaultValue} value={value} placeholder={placeholder} onChange={func as ChangeEventHandler<HTMLTextAreaElement>} />
+    <textarea className={styles.main} value={value} placeholder={placeholder} onChange={func as ChangeEventHandler<HTMLTextAreaElement>} />
   )
 }
