f6788955292ff936f84752d30d66e739c8948e01
OlekVano <olekvano@gmail.com>
2023-02-26 13:39:04 +0000

    Added NavBar responsiveness relative to the current page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -7,19 +7,40 @@ import people from '../../assets/people.png'
 import settings from '../../assets/settings.png'
 import logout from '../../assets/logout.png'
 import Line from '../Line'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../../firebase-setup'
+import { UserContext } from '../../user-context'
 
 export default function NavBar() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
   return (
-    <div className={styles.main}>
-      <NavButton text={'Home'} image={house} selected={true} roundedTop={true} />
-      <NavButton text={'Profile'} image={profile} selected={false} />
-      <NavButton text={'Messages'} image={messages} selected={false} />
-      <NavButton text={'People'} image={people} selected={false} />
-      <div style={{width: '80%', margin: '0 auto'}}>
-        <Line />
-      </div>
-      <NavButton text={'Settings'} image={settings} selected={false} />
-      <NavButton text={'Logout'} image={logout} selected={false} />
-    </div>
+    <UserContext.Consumer>
+      {function renderFromContext(ctx) {
+        return (
+          <div className={styles.main}>
+            <NavButton func={() => {navigate('/')}} text={'Home'} image={house} selected={location.pathname === '/'} roundedTop={true} />
+            <NavButton func={() => {navigate(`/profile/${ctx.currUser?.uid}`)}} text={'Profile'} image={profile} selected={location.pathname === `/profile/${ctx.currUser?.uid}`} />
+            <NavButton func={() => {navigate('/messages')}} text={'Messages'} image={messages} selected={location.pathname === '/messages'} />
+            <NavButton func={() => {navigate('/people')}} text={'People'} image={people} selected={location.pathname === '/people'} />
+            <div style={{width: '80%', margin: '0 auto'}}>
+              <Line />
+            </div>
+            <NavButton func={() => {navigate('/settings')}} text={'Settings'} image={settings} selected={location.pathname === '/settings'} />
+            <NavButton func={handleSignOut} text={'Logout'} image={logout} selected={false} />
+          </div>
+        )
+      }}
+    </UserContext.Consumer>
   )
+
+  // *************************
+
+  function handleSignOut() {
+    signOut(auth).then(function afterSignOut() {
+      navigate('/login')
+    })
+  }
 }
--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -1,15 +1,17 @@
+import { MouseEventHandler } from 'react'
 import styles from './index.module.scss'
 
 type Props = {
   image: string,
   text: string,
   selected: boolean,
-  roundedTop?: boolean
+  roundedTop?: boolean,
+  func: Function
 }
 
-export default function NavButton({ image, text, selected, roundedTop=false }: Props) {
+export default function NavButton({ image, text, selected, roundedTop=false, func=function(){} }: Props) {
   return (
-    <div className={`${styles.main} ${selected ? styles.selected : ''} ${roundedTop ? styles.roundedTop : ''}`}>
+    <div className={`${styles.main} ${selected ? styles.selected : ''} ${roundedTop ? styles.roundedTop : ''}`} onClick={func as MouseEventHandler}>
       <img src={image} className={styles.image} />
       <div className={styles.text}>{text}</div>
     </div>
