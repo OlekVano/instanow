ba583e736f54cf8b0af84a7188111ede48767d64
OlekVano <olekvano@gmail.com>
2023-02-28 15:05:28 +0000

    Finished post creation

--- src/components/CreatePostModal/index.tsx
+++ src/components/CreatePostModal/index.tsx
@@ -1,16 +1,24 @@
-import { useContext } from 'react';
+import { ChangeEvent, ChangeEventHandler, TextareaHTMLAttributes, useContext, useState } from 'react';
 import { UserContext } from '../../user-context';
 import Button from '../Button';
 import CardWrapper from '../CardWrapper';
 import Modal from '../Modal';
 import ProfilePicture from '../ProfilePicture';
 import styles from './index.module.scss'
-import picture from '../../assets/picture.png'
+import landscapeIcon from '../../assets/picture.png'
 import { ShareModalContext } from '../../share-modal-context';
+import { generateUniqueId } from '../../utils';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePostModal() {
   const userCtx = useContext(UserContext)
   const shareModalCtx = useContext(ShareModalContext)
+
+  const [pictureInputId] = useState(generateUniqueId())
+  const [picture, setPicture] = useState<string>()
+  const [text, setText] = useState('')
+
+  const navigate = useNavigate()
 
   return (
     <Modal onExit={shareModalCtx.closeModal}>
@@ -18,21 +26,72 @@ export default function CreatePostModal() {
         <div className={styles.main}>
           <div className={styles.container}>
             <ProfilePicture size='md' src={userCtx.currProfile?.profilePicture} />
-            <input placeholder="What's happening?" className={styles.input} />
+            <textarea value={text} placeholder="What's happening?" className={styles.input} onChange={manageTextChange} />
           </div>
-          <div className={styles.photoContainer}>
-            <img src={picture} className={styles.image} />
-            <div className={styles.buttonsContainer}>
-              <Button text='Upload picture' width='150px' />
-              <Button text='Take picture' width='150px' />
+          {
+            picture ? 
+            <img src={picture} className={styles.picture} />
+            :
+            <div className={styles.photoContainer}>
+              <img src={landscapeIcon} className={styles.image} />
+              <div className={styles.buttonsContainer}>
+                <Button text='Upload picture' width='150px' func={triggerImageUpload} />
+                <input id={pictureInputId} onChange={manageImageUpload} type='file' accept='image/*' style={{display: 'none'}} /> 
+                <Button text='Take picture' width='150px' />
+              </div>
             </div>
-          </div>
+          }
           <div className={styles.buttonsContainer2}>
             <Button text='Close' type={2} func={shareModalCtx.closeModal} />
-            <Button text='Post' />
+            <Button text='Post' func={createPost} />
           </div>
         </div>
       </CardWrapper>
     </Modal>
   )
+
+  // *************************************
+
+  async function createPost() {
+    if (!text && !picture) {
+      alert('Post can\'t be empty')
+    }
+
+    const json = JSON.stringify({
+      text: text,
+      picture: picture
+    })
+
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
+      method: 'POST',
+      headers: new Headers({'Authorization': `Bearer ${await userCtx.currUser?.getIdToken()}`, 'Content-Type': 'application/json'}),
+      body: json,
+    })
+
+    if (res.status === 200) navigate(`/posts/${(await res.json()).id}`)
+    else alert(`Error: ${res.statusText}`) 
+  }
+
+  function manageTextChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setText(e.target.value)
+  }
+
+  function triggerImageUpload() {
+    document.getElementById(pictureInputId)?.click()
+  }
+
+  function manageImageUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return
+    const file = e.target.files[0]
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
+    fileReader.onload = () => {
+      const image = new Image()
+      image.onload = () => {
+        setPicture(fileReader.result as string)
+      }
+      // The line below calls image.onload
+      image.src = fileReader.result as string
+    }
+  }
 }
