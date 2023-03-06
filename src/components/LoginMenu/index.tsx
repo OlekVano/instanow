63216996bb866242a2130f38060f243778e6dda1
OlekVano <olekvano@gmail.com>
2023-03-06 10:05:15 +0000

    Added login with email

--- src/components/LoginMenu/index.tsx
+++ src/components/LoginMenu/index.tsx
@@ -3,16 +3,22 @@ import { signInWithGoogle } from '../../utils'
 import Input from '../Input'
 import Button from '../Button'
 import styles from './index.module.scss'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { ChangeEvent, useState } from 'react'
+import { auth } from '../../../firebase-setup'
 
 export default function LoginMenu() {
   const navigate = useNavigate()
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className={styles.main}>
-      <Input placeholder='Email Address' />
-      <Input placeholder='Password' />
+      <Input placeholder='Email Address' value={email} func={manageEmailInput} />
+      <Input type='password' placeholder='Password' value={password} func={managePasswordInput} />
       <div className={styles.buttonsContainer}>
-        <Button text='Login' />
+        <Button text='Login' func={loginWithEmail} />
         <div>or</div>
         <Button type={2} text='Login with Google' func={loginWithGoogle} />
       </div>
@@ -20,6 +26,28 @@ export default function LoginMenu() {
   )
 
   // ***********************************
+
+  function manageEmailInput(e: ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value.trim())
+  }
+
+  function managePasswordInput(e: ChangeEvent<HTMLInputElement>) {
+    setPassword(e.target.value.trim())
+  }
+
+  function loginWithEmail() {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      navigate('/')
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  }
 
   function loginWithGoogle() {
     signInWithGoogle(function navigateToHome() {navigate('/')})
