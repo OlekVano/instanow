ec5d86f6b3e1b4865f6b009086f870d5472bc092
OlekVano <olekvano@gmail.com>
2023-03-12 17:16:56 +0000

    Fixed login and sign up inputs

--- src/components/SignUpMenu/index.tsx
+++ src/components/SignUpMenu/index.tsx
@@ -16,9 +16,15 @@ export default function SignUpMenu() {
 
   return (
     <div className={styles.main}>
-      <Input placeholder='Email Address' value={email} func={manageEmailInput} />
-      <Input type='password' placeholder='Password' value={password} func={managePasswordInput} />
-      <Input type='password' placeholder='Repeat password' value={password2} func={managePasswordInput2} />
+      <div className={styles.inputWrapper}>
+        <Input placeholder='Email Address' value={email} func={manageEmailInput} />
+      </div>
+      <div className={styles.inputWrapper}>
+        <Input type='password' placeholder='Password' value={password} func={managePasswordInput} />
+      </div>
+      <div className={styles.inputWrapper}>
+        <Input type='password' placeholder='Repeat password' value={password2} func={managePasswordInput2} />
+      </div>
       <div className={styles.buttonsContainer}>
         <Button text='Sign up' func={createEmailAccount} />
         <div>or</div>
