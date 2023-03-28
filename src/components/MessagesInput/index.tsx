9cf32c47c45cf36b873743ef32813bc77aea22cd
OlekVano <olekvano@gmail.com>
2023-03-28 10:18:04 +0100

    Added a message input

--- src/components/MessagesInput/index.tsx
+++ src/components/MessagesInput/index.tsx
@@ -1,18 +1,35 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { UserContext } from '../../user-context'
 import Button from '../Button'
+import ButtonSmall from '../ButtonSmall'
 import MultilineInput from '../MultilineInput'
 import ProfilePicture from '../ProfilePicture'
 import styles from './index.module.scss'
+import paperPlane from '../../assets/paper-plane.png'
 
 export default function MessagesInput() {
   const userCtx = useContext(UserContext)
 
+  const inputRef = useRef<HTMLDivElement>(null)
+
+  const defaultInputText = 'Send a message...'
+
   return (
     <div className={styles.main}>
-      <ProfilePicture size='s' src={userCtx.currProfile?.profilePicture} />
-      <MultilineInput defaultValue='Send a message...' />
-      <Button text='Send' />
+      <div className={styles.inputWrapper}>
+        <MultilineInput defaultValue={defaultInputText} reference={inputRef} onFocus={onInputFocus} onBlur={onInputBlur} />
+      </div>
+      <ButtonSmall image={paperPlane} />
     </div>
   )
+
+  // ***********************************
+
+  function onInputFocus() {
+    if (inputRef.current!.innerText === defaultInputText) inputRef.current!.innerText = ''
+  }
+
+  function onInputBlur() {
+    if (inputRef.current!.innerText === '') inputRef.current!.innerText = defaultInputText
+  }
 }
--- src/components/MultilineInput/index.tsx
+++ src/components/MultilineInput/index.tsx
@@ -1,16 +1,17 @@
-import { CSSProperties, FormEventHandler, ReactEventHandler, RefObject, SyntheticEvent, useEffect, useState } from 'react'
-import { generateUniqueId } from '../../utils'
+import { CSSProperties, FocusEventHandler, FormEventHandler, MouseEventHandler, ReactEventHandler, RefObject, SyntheticEvent, useEffect, useState } from 'react'
 import  styles from './index.module.scss'
 
 type Props = {
   onInput?: Function,
   defaultValue?: string,
   reference?: RefObject<HTMLDivElement>,
-  style?: CSSProperties
+  style?: CSSProperties,
+  onFocus?: Function,
+  onBlur?: Function
 }
 
-export default function MultilineInput({ onInput=function(){}, defaultValue='', reference, style }: Props) {
+export default function MultilineInput({ onInput=function(){}, defaultValue='', reference, style, onFocus=function(){}, onBlur=function(){} }: Props) {
   return (
-    <div ref={reference} style={style} className={styles.main} onInput={onInput as FormEventHandler} suppressContentEditableWarning={true} contentEditable>{defaultValue}</div>
+    <div ref={reference} style={style} onBlur={onBlur as FormEventHandler} onFocus={onFocus as FocusEventHandler} className={styles.main} onInput={onInput as FormEventHandler} suppressContentEditableWarning={true} contentEditable>{defaultValue}</div>
   )
 }
